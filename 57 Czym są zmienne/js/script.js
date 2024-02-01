4364af92070ceb90bea4905790093199d99dc9bc
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-02 00:39:43 +0100

    Exercise in const

--- 57 Czym są zmienne/js/script.js
+++ 57 Czym są zmienne/js/script.js
@@ -3,6 +3,19 @@
 // STROMG
 const color = 'niebieski'
 
-
 // NUMBER
 let age = 24
+
+console.log(age)
+
+const name = 'Mateusz'
+
+console.log(`Cześć mam na imie ${name} i mam ${age} lata `)
+
+
+// Zadanie 
+const imie = 'Mateusz'
+const jedzonko = 'naleśniki'
+let wiek = 27
+
+console.log(`Cześć, jestem ${imie}, lubię jeść ${jedzonko}. Mam ${wiek} lat.`)
