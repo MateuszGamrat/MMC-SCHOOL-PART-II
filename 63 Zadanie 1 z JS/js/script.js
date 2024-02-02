6beac086bad0b3efc4d07aa380934c4cbd0095df
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-02 01:03:04 +0100

    First exercise of js

---
+++ 63 Zadanie 1 z JS/js/script.js
@@ -0,0 +1,9 @@
+const favColor = 'red'
+const meal = 'schabowy'
+const favFirstDrink = 'pepsi'
+const firstFunction = 123
+
+const name = 'Mateusz'
+const age = 27
+
+console.log(`Cześć, jestem ${name}, mam ${age} lat.`)
