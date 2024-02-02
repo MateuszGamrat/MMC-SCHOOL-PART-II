943c6d64d5747addbe268f7cdcf02e6d6b8b2bdd
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-02 01:02:51 +0100

    How to call const

---
+++ 60 Nazywanie zmiennych/js/script.js
@@ -0,0 +1,19 @@
+// Zmienne zapisujemy po angielsku
+// camelCase - notacja wielbłądzia
+
+const name = 'Mateusz'
+
+
+const favcolor = 'niebieski'
+const facColor = 'zielony'
+
+
+// nie dajemy cyfr na początku nazwy zmiennej
+const test1 = 123
+// nie używamy znaku minusu
+// const fav-color = 'red'
+const fac_color = 'red'
+
+// zapis zarezerwowany dla klas
+const User = 'abcd'
+
