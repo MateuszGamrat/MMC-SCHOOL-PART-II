fa5d2d2783f848bd487d95962e787f688189b5d1
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-03 19:56:26 +0100

    Types of data

---
+++ 65 Typy danych w JS/js/script.js
@@ -0,0 +1,20 @@
+// ===================
+//  TYPY DANYCH
+// ===================
+
+// *** TYPY PROSTE ***
+
+// STRING - TEXT
+const name='Mateusz'
+
+// NUMBER - LICZBY
+const age = 27
+
+// BOOLEAN - PRAWDA / FAŁSZ
+const isTrue = true
+
+// NULL & UNDEFINED
+const test = null
+let test2
+console.log(test1)
+console.log(test2)
