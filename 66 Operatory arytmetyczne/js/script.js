449f8889f64c726e98b625ab486025b374c32e2d
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-06 23:34:23 +0100

    Math in JS

---
+++ 66 Operatory arytmetyczne/js/script.js
@@ -0,0 +1,23 @@
+const add = 5 + 6
+console.log(`Wynik dodawania: ${add}`);
+
+const substract = 15 - 6
+console.log(`Wynik odejmowania: ${substract}`);
+
+const multyply = 5 * 6
+console.log(`Wynik mnożenia: ${multyply}`);
+
+const divide = 30 / 6
+console.log(`Wynik dzielenia: ${divide}`);
+
+const modulo = 30 % 12
+console.log(`Wynik modulo: ${modulo}`);
+
+let num = 4
+console.log(num);
+num++
+num++
+num++
+console.log(num);
+num--
+console.log(num);
