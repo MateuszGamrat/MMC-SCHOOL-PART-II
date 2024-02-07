9a682356691a302dd2306e108110410ddd3e6d0d
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-07 21:14:09 +0100

    Comparison operators

---
+++ 68 Operatory porównania/js/script.js
@@ -0,0 +1,25 @@
+const x = 8
+const y ="8"
+const z = 9
+
+
+/*
+    == sprawdzamy czy coś jest RÓWNE (sama wartość)
+    === sprawdzamy czy coś jest RÓWNE (wartość i typ danych)
+
+    != sprawdzamy czy coś jest RÓŻNE (sama wartość)
+    !== sprawdzamy czy coś jest RÓŻNE (wartość i typ danych)
+
+    > wiekszy niż
+    < mniejszy niż
+
+    >= większy lub równy
+    <= mniejszy lub równy
+
+*/
+
+if(x  y){
+    console.log("true")
+}else{
+    console.log("folse");
+}
