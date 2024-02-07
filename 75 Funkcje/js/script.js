03c2606edbc0e139567a7259915a7f84671a8c68
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-07 22:34:14 +0100

    Function

---
+++ 75 Funkcje/js/script.js
@@ -0,0 +1,27 @@
+function test(){
+console.log("Moja pierwsza funkcja");
+
+}
+
+test()
+
+function addNumbers(x,y){
+    console.log(x+y);
+
+}
+
+addNumbers(5, 8)
+
+function showName(name){
+    console.log(`Podane imie to: ${name}`);
+}
+
+showName('Klaudia')
+
+
+function multiply(x,y){
+    console.log(x * y);
+}
+
+
+multiply(2,3)
