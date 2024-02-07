f93c99764c02bda3a74c6fd2c65982116fc91cb8
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-07 22:08:21 +0100

    Array list

---
+++ 73 Tablice/js/script.js
@@ -0,0 +1,8 @@
+const colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+
+console.log(colors);
+console.log(colors[2]);
+console.log(colors.length);
+
+colors.push('gold')
+console.log(colors);
