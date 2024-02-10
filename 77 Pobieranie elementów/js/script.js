34162448a57fbc91ffd6e3309b462713018d67b2
Mateusz Gamrat <mattgamrat@gmail.com>
2024-02-10 14:15:33 +0100

    QuerySelection from JS

---
+++ 77 Pobieranie elementów/js/script.js
@@ -0,0 +1,13 @@
+const p = document.querySelector('p.test')
+console.log(p);
+
+
+const btn = document.querySelector('#btn')
+console.log(btn);
+
+const liItems = document.querySelectorAll('li')
+console.log(liItems);
+console.log(liItems[2]);
+
+const AllParagraph = document.querySelectorAll('p')
+console.log(AllParagraph);
